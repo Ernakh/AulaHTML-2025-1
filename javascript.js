868884515c26574f866e0156ca85b08fc467d928
Fabrício <fabriciotonettolondero@gmail.com>
2025-04-08 21:52:47 -0300

    aula js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,42 @@
-alert('Oi!');
+// var numero = 1;
+
+// // Comparação normal (compara apenas os valores)
+// if (numero == "1") { // resultado: true
+//     alert("== compara apenas os valores");
+// }
+
+// // Comparação restrita (compara valores e tipos das variáveis)
+// var mensagem;
+// if (numero === "1") { // resultado: false
+//     mensagem = "Nunca executa";
+// } else {
+//     mensagem = "Number é diferente de String com o ===";
+// }
+// alert(mensagem);
+
+// var x = (numero === "1") ? "Nunca executa" : "Number é diferente de String com o ===";
+// alert(X);
+
+// // FOR - executa um laço um determinado número de vezes
+// var pessoas = ["João", "José", "Maria", "Sebastião", "Antônio"];
+
+// for (var i = 0; i < pessoas.length; i++) {
+//     alert(pessoas[i]);
+// }
+
+// // FOR IN - executa um laço nas propriedades de um objeto
+// var nome = "";
+// var pessoa = { nome: "Iara", sobrenome: "Almeida" };
+
+// for (x in pessoa) {
+//     nome += pessoa[x] + " ";
+// }
+
+// alert(nome);
+
+
+function alertas()
+{
+    alert('Primeiro alerta!');
+    alert('Segundo alerta!');
+}
